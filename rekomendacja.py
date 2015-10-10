b09e16f24a222d704de84f7612149698f80a1b88
marlesz <leszczuk.marta@gmail.com>
2015-10-10 18:30:24 +0200

    dokonano zmiany

--- rekomendacja.py
+++ rekomendacja.py
@@ -1,47 +1,49 @@
-#  Wzorowane na przykładzie Rona Zacharskiego
-
-
-from math import sqrt
-
-users = {"Ania": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
-         "Bonia":{"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
-         "Celina": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
-         "Dominika": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0},
-         "Ela": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
-         "Fruzia":  {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
-         "Gosia": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
-         "Hela": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
-        }
-
-
-
-def manhattan(rating1, rating2):
-    """Oblicz odległość w metryce taksówkowej między dwoma  zbiorami ocen
-       danymi w postaci: {'The Strokes': 3.0, 'Slightly Stoopid': 2.5}
-       Zwróć -1, gdy zbiory nie mają wspólnych elementów"""
-       
-    # TODO: wpisz kod
-    pass
-
-
-def computeNearestNeighbor(username, users):
-    """dla danego użytkownika, zwróć listę innych użytkowników według bliskości preferencji"""
-    distances = []
-    # TODO: wpisz kod
-    return distances
-
-def recommend(username, users):
-    """Zwróć listę rekomendacji dla użytkownika"""
-    # znajdź preferencje najbliższego sąsiada
-    nearest = computeNearestNeighbor(username, users)[0][1]
-
-    recommendations = []
-    # zarekomenduj użytkownikowi wykonawcę, którego jeszcze nie ocenił, a zrobił to jego najbliższy sąsiada
-    # TODO: wpisz kod
-    # using the fn sorted for variety - sort is more efficient
-    return sorted(recommendations, key=lambda artistTuple: artistTuple[1], reverse = True)
-
-# przykłady
-
-print( recommend('Hela', users))
-#print( recommend('Celina', users))
+###  Wzorowane na przykładzie Rona Zacharskiego
+##
+##
+##from math import sqrt
+##
+##users = {"Ania": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
+##         "Bonia":{"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
+##         "Celina": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
+##         "Dominika": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0},
+##         "Ela": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
+##         "Fruzia":  {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
+##         "Gosia": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
+##         "Hela": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
+##        }
+##
+##
+##
+##def manhattan(rating1, rating2):
+##    """Oblicz odległość w metryce taksówkowej między dwoma  zbiorami ocen
+##       danymi w postaci: {'The Strokes': 3.0, 'Slightly Stoopid': 2.5}
+##       Zwróć -1, gdy zbiory nie mają wspólnych elementów"""
+##       
+##    # TODO: wpisz kod
+##    pass
+##
+##
+##def computeNearestNeighbor(username, users):
+##    """dla danego użytkownika, zwróć listę innych użytkowników według bliskości preferencji"""
+##    distances = []
+##    # TODO: wpisz kod
+##    return distances
+##
+##def recommend(username, users):
+##    """Zwróć listę rekomendacji dla użytkownika"""
+##    # znajdź preferencje najbliższego sąsiada
+##    nearest = computeNearestNeighbor(username, users)[0][1]
+##
+##    recommendations = []
+##    # zarekomenduj użytkownikowi wykonawcę, którego jeszcze nie ocenił, a zrobił to jego najbliższy sąsiada
+##    # TODO: wpisz kod
+##    # using the fn sorted for variety - sort is more efficient
+##    return sorted(recommendations, key=lambda artistTuple: artistTuple[1], reverse = True)
+##
+### przykłady
+##
+##print( recommend('Hela', users))
+###print( recommend('Celina', users))
+print ("Nazywam się Marta Leszczuk. Mój login Github to marlesz. \nTemat mojej pracy dyplomowej to: \"Kontekstowa generalizacja konturów zabudowy z wykorzystaniem narzędzi morfologii matematycznej\".")
+print ("Powyższy skrypt zawiera słownik użytkowników wraz z ich ocenami poszczególnych artystów muzycznych. Zdefiniowane są w nim funkcje, które z założenia mają odpowiednio: obliczyć odległość w metryce Manhattan; wyszukać sąsiadów w oparciu o preferencje muzyczne; zwrócić rekomendacje muzyczne. Powyższe funkcje jednak aktualnie nie wykazują działania, ponieważ nie mają wpisanych instrukcji, wykonujących powyżej opisane zadania. Wynikiem uruchomienia skryptu jest wyświetlenie rekomendacii dla konkretnych użytkowników")
